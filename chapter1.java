2a7c667d7aa38cc52074eb220af88ca1250c083c
hitoron-0924 <hitoron.0924@gmail.com>
2018-07-01 21:12:37 +0900

    chapter2:変数

---
+++ chapter1.java
@@ -0,0 +1,13 @@
+class hello{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
+class say{
+    public static void main(String args[]){
+        System.out.println("say");
+        System.out.print("hello ");
+        System.out.println("and ");
+        System.out.println("name ");
+    }
+}
---
+++ chapter2.java
@@ -0,0 +1,26 @@
+ class VariableExample{
+    public static void main(String[] args){
+        int i;
+        i = 5;
+        System.out.println(i);
+        i = 10;
+        System.out.println(i);
+    }
+}
+class TypeExample{
+    public static void main(String[] args){
+        double d;
+        d = 9.9;
+        char c;
+        c = 'a';
+        byte b;
+        b = 81;
+        boolean bool;
+        bool = true;
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(bool);
+    }
+}
+ 
