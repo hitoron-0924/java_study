63aeef4f56f183ad2853c349fc04efb540264bdb
hitoron-0924 <hitoron.0924@gmail.com>
2018-07-02 22:58:31 +0900

    chapter3 end

--- chapter3.java
+++ chapter3.java
@@ -1,8 +1,37 @@
+
+class CalcExample{
+    public static void main(String[] args){
+        System.out.println(2 + 3);
+    }
+}
+
 class CalcExample2{
     public static void main(String[] args){
         int i = 10;
         int j = i * 2;
         System.out.println("i is " + i);
         System.out.println("j is " + j);
+    }
+}
+class CalcExample3{
+    public static void main(String[] args){
+        int i;
+        i = 11;
+        i++;
+        i /= 2;
+        System.out .println("i is " + i);
+
+        int j;
+        j = i * i;
+        System.out.println("j is " + j);
+
+        j = i++;
+        System.out.println("frist increment " + j);
+        System.out.println("i is " + i);
+        
+        j = ++i;
+        System.out.println("second increment " + j);
+        System.out.println("i is " + i);
+
     }
 }
