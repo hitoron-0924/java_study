96ae01527900c2cb35a6f6cf3e309062f60024d5
hitoron-0924 <hitoron.0924@gmail.com>
2018-07-05 23:55:05 +0900

    lesson3の追加

---
+++ Lesson3/chapter1.java
@@ -0,0 +1,21 @@
+class IfExample{
+    public static void main(String[] args){
+        int age;
+        age = 18;
+        if(age < 20){
+            System.out.println("under 20");
+        }
+    }
+}
+class  ElseExample{
+    public static void main(String[] args){
+        int age;
+        age = 20;
+        if(age < 20){
+            System.out.println("under 20");
+        }
+        else{
+            System.out.println("over 20");
+        }
+    }
+}
